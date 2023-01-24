7406d63e45f3ea4b08d45154c71fdb7d16c3a6df
itsme-v07 <geansyahip30@gmail.com>
2023-01-24 08:25:52 +0700

    show meal_item

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  const MealDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('The meal!'),
+    );
+  }
+}
