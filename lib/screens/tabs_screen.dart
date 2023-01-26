67f49db495c2d654a8b4b695657ec5a4d50e8a38
itsme-v07 <geansyahip30@gmail.com>
2023-01-26 07:44:44 +0700

    adding drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -40,6 +40,9 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[_selectedPageIndex]['title'] as String),
       ),
+      drawer: Drawer(
+        child: Text('drawer!'),
+      ),
       body: _pages[_selectedPageIndex]['page'] as Widget,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
