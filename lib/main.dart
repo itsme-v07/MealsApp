107d421d8c16ef1bd3d47c4b10739b2db21eca67
itsme-v07 <geansyahip30@gmail.com>
2023-02-04 11:06:22 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyAppState extends State<MyApp> {
         _favoriteMeals.indexWhere((meal) => meal.id == mealId);
     if (existingIndex >= 0) {
       setState(() {
-        _favoriteMeals.remove(existingIndex);
+        _favoriteMeals.removeAt(existingIndex);
       });
     } else {
       setState(() {
--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -3,31 +3,44 @@ import 'package:flutter/material.dart';
 import '../models/meal.dart';
 import '../widgets/meal_item.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
   final List<Meal> favoriteMeals;
 
   FavoritesScreen(this.favoriteMeals);
 
   @override
+  State<FavoritesScreen> createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  // List<Meal> meals = [];
+
+  // @override
+  // void initState() {
+  //   meals = widget.favoriteMeals;
+  //   super.initState();
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    if (favoriteMeals.isEmpty) {
+    if (widget.favoriteMeals.isEmpty) {
       return Center(
-        child: Text('Favorites!'),
+        child: Text('You have no favorites yet'),
       );
     } else {
       return ListView.builder(
         itemBuilder: (ctx, index) {
           return MealItem(
-            id: favoriteMeals[index].id,
-            title: favoriteMeals[index].title,
-            imageUrl: favoriteMeals[index].imageUrl,
-            duration: favoriteMeals[index].duration,
-            complexity: favoriteMeals[index].complexity,
-            affordability: favoriteMeals[index].affordability,
+            id: widget.favoriteMeals[index].id,
+            title: widget.favoriteMeals[index].title,
+            imageUrl: widget.favoriteMeals[index].imageUrl,
+            duration: widget.favoriteMeals[index].duration,
+            complexity: widget.favoriteMeals[index].complexity,
+            affordability: widget.favoriteMeals[index].affordability,
             // removeItem: _removeMeal,
           );
         },
-        itemCount: favoriteMeals.length,
+        itemCount: widget.favoriteMeals.length,
       );
     }
   }
